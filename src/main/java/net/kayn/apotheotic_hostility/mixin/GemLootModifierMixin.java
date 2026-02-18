f0729501d7cc4df46543d232670ab5b0bf44de29
Kayne <aron.udvardi@gmail.com>
2026-02-18 23:18:44 +0100

    Disabled Apotheosis GemLootModifier via mixin

---
+++ src/main/java/net/kayn/apotheotic_hostility/mixin/GemLootModifierMixin.java
@@ -0,0 +1,18 @@
+package net.kayn.apotheotic_hostility.mixin;
+
+import dev.shadowsoffire.apotheosis.adventure.loot.GemLootModifier;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = GemLootModifier.class, remap = false)
+public class GemLootModifierMixin {
+    @Inject(method = "doApply", at = @At("HEAD"), cancellable = true)
+    private void cancelGemInjection(ObjectArrayList<ItemStack> generatedLoot, LootContext context, CallbackInfoReturnable<ObjectArrayList<ItemStack>> cir) {
+        cir.setReturnValue(generatedLoot);
+    }
+}
